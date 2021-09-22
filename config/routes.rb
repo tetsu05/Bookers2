15b147a455e4ffd7dd94809058fd375521e48599
tetsu05 <tetsu051955@gmail.com>
2021-09-22 07:26:10 +0000

    [Middle]28

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :homes, only: [:new, :create, :index, :show, :destroy]
+  resources :homes, only: [:new, :index, :show]
   resources :users, only: [:index, :show]
   
 end
