5e3d083eb53460f39b860d66d54db94e2b64ed66
tetsu05 <tetsu051955@gmail.com>
2021-09-26 09:54:31 +0000

    [Middle]33

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
 
   def index
+    @user = current_user
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1e50b42b5703496598737127280f45a4aa8bc1b893b4d3cebdf0c331570e29c5fef29883707aef481bbd16e360d1d3cd1c3c51f91f8e9f078c45fe85f141bdc6'
