fe4788f77c8f9ab1c26ab0f856c431ff9c037c7d
tetsu05 <tetsu051955@gmail.com>
2021-09-26 04:33:26 +0000

    [Middle]30

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,12 @@
 class BooksController < ApplicationController
-  
+
   def create
-  @book = Book.new(book_params)
-  @book.user_id = current_user.id
-  @book.save
+   @book = Book.new(book_params)
+   @book.user_id = current_user.id
+   @book.save
   redirect_to root_path
   end
-  
+
 
   def index
     @user = current_user
@@ -16,24 +16,25 @@ class BooksController < ApplicationController
 
   def show
   end
-  
+
   def edit
+    @user = User.find(params[:id])
   end
-  
+
   def update
   end
 
   def destroy
   end
-  
+
   private
   def book_params
-    params.require(:book).permit(:image, :title, :body) 
+    params.require(:book).permit(:image, :title, :body)
   end
-  
+
   #private
   #def book_params
   #  params.require(:book).permit(:title, :body)
  # end
-  
+
 end
